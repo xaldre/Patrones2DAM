ecb68a9a98e9695384ae1417b7e4668526a0ba60
Albano <aandreuc01@gmail.com>
2017-11-24 19:51:28 +0100

    Mejorada v3

--- singleton/v3/multipleInstances/Cliente.java
+++ singleton/v3/multipleInstances/Cliente.java
@@ -3,14 +3,14 @@ package v3.multipleInstances;
 public class Cliente {
 
 	private void pedirPizza() {
-		for(int i=0; i<10; i++){
-			System.out.println("Cliente: Quisiera una pizza, por favor.");
-			SingletonPizza.instance().getPizza();
-		}
+		System.out.println("Cliente: Quisiera una pizza, por favor.");
+		SingletonPizza.instance().getPizza();
 	}
-	
-	public static void main(String[] args){
-		new Cliente().pedirPizza();
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			new Cliente().pedirPizza();
+		}
 	}
 
 }
